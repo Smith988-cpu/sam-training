6c618cd4344d27ccb370dca52aa20cfcfb88a55c
azuretraining1111@gmail.com <azuretraining1111@gmail.com>
2025-06-26 06:00:49 +0000

    TEST

--- models/silver/fact_orders_silver.sql
+++ models/silver/fact_orders_silver.sql
@@ -1,5 +1,12 @@
+{{config(
+    materialized= 'view',
+    database= 'SILVER',
+    schema= 'PUBLIC')}} 
+
 select 
-* from 
+o.ORDER_ID,o.CUSTOMER_ID,
+FIRST_NAME || ' ' || LAST_NAME as CUSTOMERNAME,
+o.ORDER_DATE,O.TOTAL_AMOUNT from 
 {{ref("fact_orders")}} o join
 {{ref("dim")}} c on o.CUSTOMER_ID=c.CUSTOMER_ID
 left join {{ref("country_codes")}} cc on cc.COUNTRY=c.COUNTRY
