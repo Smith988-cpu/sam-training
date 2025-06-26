beea921aebb1af8ade381df0eddec04097bebe88
azuretraining1111@gmail.com <azuretraining1111@gmail.com>
2025-06-26 04:18:29 +0000

    test

--- models/gold/dimagg.sql
+++ models/gold/dimagg.sql
@@ -3,5 +3,7 @@
     database= 'GOLD',
     schema= 'PUBLIC'
 )}}
-select count(*) as total_customers
+select country,count(*) as total_customers
 from {{ref("dimsilver")}}
+group by country
+order by total_customers desc
