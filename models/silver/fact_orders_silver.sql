7f7d373208c17f9ae0d61fb5a0e6091d633c919c
azuretraining1111@gmail.com <azuretraining1111@gmail.com>
2025-06-26 05:28:49 +0000

    ASDSADSAD

--- models/silver/fact_orders_silver.sql
+++ models/silver/fact_orders_silver.sql
@@ -1,3 +1,5 @@
-select * from 
-{{ref("fact_orders")}} o
+select 
+* from 
+{{ref("fact_orders")}} o join
 {{ref("dim")}} c on o.CUSTOMER_ID=c.CUSTOMER_ID
+left join {{ref("country_codes")}} cc on cc.COUNTRY=c.COUNTRY
