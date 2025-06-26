e410b28ba4cc14d676ebf0e3b50f4fc5194c1731
azuretraining1111@gmail.com <azuretraining1111@gmail.com>
2025-06-26 06:26:06 +0000

    fdgfdgfd

--- models/gold/customer_order_summary.sql
+++ models/gold/customer_order_summary.sql
@@ -4,4 +4,4 @@ SUM(TOTAL_AMOUNT) as amount,
 count(ORDER_ID) as total_orders
 from {{ref("fact_orders_silver")}} f
 left join {{ref("dimproduct")}} d on f.PRODUCT_ID=d.PRODUCT_ID
-group by f. CUSTOMER_ID f.CUSTOMERNAME
+group by f.CUSTOMER_ID, f.CUSTOMERNAME
