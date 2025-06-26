b6a19eaf148c7bfbdd78b13b74ea7934a565fe9d
azuretraining1111@gmail.com <azuretraining1111@gmail.com>
2025-06-26 04:26:41 +0000

    TEST

--- models/silver/dimsilver.sql
+++ models/silver/dimsilver.sql
@@ -1,13 +1,12 @@
 {{config(
     materialized= 'view',
     database= 'SILVER',
-    schema= 'PUBLIC'
-)}} 
+    schema= 'PUBLIC')}} 
 with cte as(
 select 
-CUSTOMER_ID,FIRST_NAME,LAST_NAME,EMAIL,
+CUSTOMER_ID,FIRST_NAME,LAST_NAME,EMAIL,COUNTRY,
 row_number() over(partition by EMAIL order by CUSTOMER_ID desc) as dedup
 from {{ref("dim")}}
 )
 select 
-CUSTOMER_ID,FIRST_NAME,LAST_NAME,EMAIL,dedup from cte where dedup=1 
+CUSTOMER_ID,FIRST_NAME,LAST_NAME,EMAIL,COUNTRY,dedup from cte where dedup=1 
